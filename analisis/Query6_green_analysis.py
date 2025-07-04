733513a166879c0a06886091a8f8ee70ed18609f
Asfa-Asfialana <as.asfialn@gmail.com>
2025-07-04 16:00:12 +0700

    Delete analisis directory

--- analisis/Query6_green_analysis.py
+++
@@ -1,8 +0,0 @@
-# green_analysis.py
-
-def compute_co2_efficiency(co2_reduction, investment_cost):
-    try:
-        efficiency = co2_reduction / (investment_cost * 1_000)
-        return round(efficiency, 2)
-    except ZeroDivisionError:
-        return "Cannot compute"
